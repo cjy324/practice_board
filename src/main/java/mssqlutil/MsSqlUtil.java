f553612a052eff067dd4dff07bdbcddddebd3f95
cjy324 <juy32400@gmail.com>
2021-08-04 15:06:59 +0900

    21.08.04 DB통신 로직 주석 추가

--- src/main/java/mssqlutil/MsSqlUtil.java
+++ src/main/java/mssqlutil/MsSqlUtil.java
@@ -18,17 +18,19 @@ public class MsSqlUtil {
 	
     public static void main(String[] args) throws ClassNotFoundException {
         Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");  
-              
     }
     
-    public void DBconnectionStart() throws SQLException {
-    	String connectionUrl = "jdbc:sqlserver://127.0.0.1:1433;"
-    						+ "database=TEST;"
-    						+ "integratedSecurity=true;";
+    // SQLserver와 연결 시작
+    private void DBconnectionStart() throws SQLException {
+    	// 참고: https://lab-502.tistory.com/4
+    	String connectionUrl = "jdbc:sqlserver://127.0.0.1:1433;"  // TCP 포트
+    						+ "database=TEST;"  // 연결할 DB
+    						+ "integratedSecurity=true;";  // windows 인증일 경우 
 		con = DriverManager.getConnection(connectionUrl);
 		System.out.println("MS-SQL 서버 접속에 성공했습니다.");
 	}
     
+    // SQLserver와 연결 종료
     private void DBconnectionEnd() throws SQLException {
     	rs.close();
         stmt.close();
@@ -36,19 +38,17 @@ public class MsSqlUtil {
         System.out.println("MS-SQL 서버 연결을 종료했습니다.");
 	}
 
+    // 게시물 리스트 가져오기
 	public List<Article> select(String sql) throws SQLException {
 		DBconnectionStart();
-        // https://lab-502.tistory.com/4
+        
 		List<Article> articles = new ArrayList<>();
-		
-		stmt = con.createStatement();
-        
-        
+
         /* executeQuery vs executeUpdate*/
         // 출처: https://aricode.tistory.com/9 [아리의 코딩 모험]
         // - executeQuery(String sql): 조회문(select, show 등)을 실행할 목적으로 사용
         // - executeUpdate(String sql): create, drop, insert, delete, update 등등 문을 처리할 때 사용
-
+		stmt = con.createStatement();
         rs = stmt.executeQuery(sql);
  
     	while (rs.next()) {
@@ -61,6 +61,7 @@ public class MsSqlUtil {
     			Article article = new Article(id, regDate, writer, title, body);
     			articles.add(article);
     	}
+    	
     	DBconnectionEnd();
 
 		return articles;	
